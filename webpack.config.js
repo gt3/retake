73d43dfa4239a2dbf791c16992a4bd05d68d2cbd
_gt3 <gt3.someday@outlook.com>
2016-08-17 01:50:57 -0500

    switched to commonjs for node module

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,18 @@ var webpack = require('webpack');
 var path = require('path');
 const minify = process.argv.indexOf('--env.compress') !== -1;
 var uglify = new webpack.optimize.UglifyJsPlugin({compress: {warnings: true, drop_console: true, dead_code: false, conditionals: false}});
-var entry = './src/index.js', plugins = [], filename = 'retake.js'
+var entry = './src/index.js', plugins = [], filename = 'retake.js', libraryTarget = 'commonjs2';
 if(minify) {
     entry = './lib/retake.es5.js';
     plugins.push(uglify);
     filename = 'retake.es5.min.js';
+    libraryTarget = 'umd';
 }
+var output = { path: './lib/', filename: filename, library: 'retake', libraryTarget: libraryTarget, umdNamedDefine: true }
+
 module.exports = {
     devtool: "source-map",
     entry: entry,
-    output: {
-        path: './lib/',
-        filename: filename,
-        library: 'retake',
-        libraryTarget: 'umd',
-        umdNamedDefine: true
-    },
+    output: output,
     plugins: plugins
 };
