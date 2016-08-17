08326eee8c5bedf5e912d86addc5a21b1b6e54b6
_gt3 <gt3.someday@outlook.com>
2016-08-17 01:27:52 -0500

    build process fixed for es5 and es5.min file size

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,22 @@
 var webpack = require('webpack');
 var path = require('path');
-const babelify = process.argv.indexOf('--env.transpile-compress') !== -1;
+const minify = process.argv.indexOf('--env.compress') !== -1;
 var uglify = new webpack.optimize.UglifyJsPlugin({compress: {warnings: true, drop_console: true, dead_code: false, conditionals: false}});
-var plugins = [], loaders = [], filename = 'retake.js';
-if(babelify) {
+var entry = './src/index.js', plugins = [], filename = 'retake.js'
+if(minify) {
+    entry = './lib/retake.es5.js';
     plugins.push(uglify);
     filename = 'retake.es5.min.js';
-    loaders.push({ test: /\.js$/, exclude: /node_modules/, loader: 'babel' })
 }
 module.exports = {
     devtool: "source-map",
-    entry: './src/index.js',
+    entry: entry,
     output: {
         path: './lib/',
         filename: filename,
         library: 'retake',
         libraryTarget: 'umd',
         umdNamedDefine: true
-    },
-    module: {
-      loaders: loaders
     },
     plugins: plugins
 };
