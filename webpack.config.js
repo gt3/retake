624c05463e2a51fea50efa8f481971edb9e954ac
Ankit Patel <gt3.someday@outlook.com>
2016-08-17 12:58:14 -0500

    reverted back to using umd

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,11 @@ var webpack = require('webpack');
 var path = require('path');
 const minify = process.argv.indexOf('--env.compress') !== -1;
 var uglify = new webpack.optimize.UglifyJsPlugin({compress: {warnings: true, drop_console: true, dead_code: false, conditionals: false}});
-var entry = './src/index.js', plugins = [], filename = 'retake.js', libraryTarget = 'commonjs2';
+var entry = './src/index.js', plugins = [], filename = 'retake.js', libraryTarget = 'umd';
 if(minify) {
     entry = './lib/retake.es5.js';
     plugins.push(uglify);
     filename = 'retake.es5.min.js';
-    libraryTarget = 'umd';
 }
 var output = { path: './lib/', filename: filename, library: 'retake', libraryTarget: libraryTarget, umdNamedDefine: true }
 
